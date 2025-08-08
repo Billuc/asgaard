7aeac73621855f65b769806c4d986ae6737b14f6
Luc Billaud <billuc@hotmail.fr>
2025-08-08 14:18:47 +0200

    fix: there was one too many slashes

--- src/lib/routes/routes.ts
+++ src/lib/routes/routes.ts
@@ -45,7 +45,7 @@ export const ROUTES: { [k in Routes]: { name: string; path: string; link: NavLin
 };
 
 export function asHref(route: Routes, params: Record<string, string> = {}): string {
-  let href = base + '/' + ROUTES[route].path;
+  let href = base + ROUTES[route].path;
   if (Object.keys(params).length > 0) {
     const query = new URLSearchParams(params).toString();
     href += `?${query}`;
