f2ae0a88df94d15b95b410eb2b929c0ab01b90d2
Billuc <luc.billaud.pro@gmail.com>
2025-08-09 15:20:31 +0200

    fix(nav): fixed active link not changing

--- src/lib/routes/NavBar.svelte
+++ src/lib/routes/NavBar.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
-	import { asHref, NAV_LINKS, Routes, ROUTES } from './routes';
+	import { asHref, NAV_LINKS, NavLink, Routes, ROUTES } from './routes';
 	import { page } from '$app/state';
 
-	const linkForRoute = Object.entries(ROUTES).find(
-		(route) => asHref(route[0] as unknown as Routes) === page.url.pathname
-	)?.[1].link;
+	function isActive(link: NavLink) {
+		const linkForRoute = Object.entries(ROUTES).find(
+			(route) => asHref(route[0] as unknown as Routes) === page.url.pathname
+		)?.[1].link;
+		return linkForRoute === link;
+	}
 </script>
 
 <div class="dock z-50 flex bg-base-200 md:hidden">
 	{#each Object.entries(NAV_LINKS) as [link, data] (link)}
-		<a href={asHref(data.route)} class={{ 'dock-active': linkForRoute === link }}>
+		<a href={asHref(data.route)} class={{ 'dock-active': isActive(link as NavLink) }}>
 			<span class="dock-label">{data.name}</span>
 		</a>
 	{/each}
@@ -23,7 +26,7 @@
 		<ul class="menu menu-horizontal px-1">
 			{#each Object.entries(NAV_LINKS) as [link, data] (link)}
 				<li>
-					<a href={asHref(data.route)} class={{ 'menu-active': linkForRoute === link }}>
+					<a href={asHref(data.route)} class={{ 'menu-active': isActive(link as NavLink) }}>
 						<span>{data.name}</span>
 					</a>
 				</li>
