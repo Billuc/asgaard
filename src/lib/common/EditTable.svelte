f216f90ea87acd363ff50977b7dd22dcb1e79049
Billuc <luc.billaud.pro@gmail.com>
2025-11-11 08:18:16 +0100

    [wip] feat: starting edit table

---
+++ src/lib/common/EditTable.svelte
@@ -0,0 +1,18 @@
+<script lang="ts" generics="Item extends { id: string }">
+interface Props {
+  items: Item[];
+  columns: {[k: keyof Item]: string};
+
+}
+</script>
+
+	<table class="edit-table table table-xs">
+		<thead>
+			<tr>
+			</tr>
+		</thead>
+		<tbody>
+				<tr>
+				</tr>
+		</tbody>
+	</table>
